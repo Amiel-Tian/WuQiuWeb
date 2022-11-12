27717826ad30f929177d8fd3c147791eb22182d8
燚 <https://gitee.com/amiel_tian>
2022-11-12 21:19:33 +0800

    路由名

--- src/router/model/renwushu/index.js
+++ src/router/model/renwushu/index.js
@@ -2,7 +2,7 @@ export default [
     {
         name: 'renwushu/index',
         path: '/renwushu/index',
-        meta: { title: '返回', icon: '' },
+        meta: { title: '任务书', icon: '' },
         components: {
             //index为声明的router-view的name
             index: () =>
