690f905c7e9b12208c0d64ae5cb3fd2e8102458a
燚 <https://gitee.com/amiel_tian>
2022-12-09 10:20:00 +0800

    集成markdown编辑器

---
+++ src/views/markdown/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <!--
+  {{ proxy.$tools.selectDictLabel(proxy.$appConfig.STATUS,scope.row.status) }}
+  v-permission="['sys:dict:update']"
+  -->
+  <v-md-editor v-model="text" height="400px"
+               :disabled-menus="[]"
+               @upload-image="handleUploadImage"
+  ></v-md-editor>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        <span>展示效果</span>
+      </div>
+    </template>
+    <v-md-preview :text="text"></v-md-preview>
+  </el-card>
+</template>
+
+<script>
+import {ref,unref, getCurrentInstance, watch, reactive, onMounted} from "vue";
+import {ElMessage, ElMessageBox} from 'element-plus';
+import {useRouter} from "vue-router";
+
+export default {
+  name: "index",
+  props:[],
+  emits:[],
+  components:{
+  },
+  setup(props ,content){
+    const router = useRouter()
+    const {proxy} = getCurrentInstance();
+    let data = {
+      text: ref('* Hello World!')
+    }
+    //监听
+    watch(() => [props.info], ([newInfo],[oldInfo]) => {
+
+    })
+    onMounted(async () => {
+
+    })
+    let methods = {
+      handleUploadImage(event, insertImage, files) {
+        // 拿到 files 之后上传到文件服务器，然后向编辑框中插入对应的内容
+        console.log(files);
+
+        // 此处只做示例
+      }
+    }
+
+    return {
+      proxy,
+      router,
+      ...data,
+      ...methods
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
